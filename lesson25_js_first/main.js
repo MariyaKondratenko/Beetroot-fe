dcd93189897b7fdda4a6c31072d9acc4a7dbf784
MariyaKondratenko <mariya.moko.m@gmail.com>
2023-08-09 12:01:01 +0200

    Основи JavaScript

---
+++ lesson25_js_first/main.js
@@ -0,0 +1,49 @@
+// Зберігати ім’я та прізвище в змінній:
+
+// const firstName = "Mariia";
+// const lastName = "Kondratenko";
+// const fullName = `${firstName} ${lastName}`;
+// console.log(`${firstName} ${lastName}`);
+// const age = 30;
+
+// Неправильних імена:
+// const 1name = "Mariia";
+// const email#
+// const @mail
+// const name user
+// const user-name
+
+// Норма
+// 1. Запитай ім’я користувача та виведи у відповідь “Привіт, *ім’я*”;
+// const userName = prompt("Enter your name");
+// console.log(`Привіт, ${userName}`);
+
+// 2. Запитай рік народження користувача, порахуй його/її вік і виведи результат. Поточний рік вкажи в коді як константу;
+// const userYearOfBirth = prompt("Enter your year of birth");
+// const currentYear = 2023;
+// const userAge = currentYear - userYearOfBirth;
+// console.log(`Вік користувача: ${userAge}`);
+
+// 3. Запитай у користувача довжину сторони квадрату і виведи периметр цього квадрата.
+// const lengthSideSquare = prompt("Введіть довжину сторони квадрата");
+// const perimeteSquare = lengthSideSquare * 4;
+// console.log(`Периметр квадрата: ${perimeteSquare}`);
+
+// Максимум
+// 1. Запитай у користувача радіус кола і виведи площу такої окружності.
+// const circleRadius = prompt("Введіть радіус кола");
+// const numberPi = 3.14;
+// const areaСircle = numberPi * (circleRadius ** 2);
+// console.log(`Площа круга: ${areaСircle}`);
+
+// 2. Запитай у користувача відстань в кілометрах між двома містами і за скільки годин він хоче дістатися. Порахуй швидкість, з якою необхідно рухатися, щоб встигнути вчасно.
+// const distanceInKilometers = +prompt("Enter distance in kilometers");
+// const howManyHours = +prompt("Enter how many hours");
+// const speed = distanceInKilometers / howManyHours;
+// console.log(`Required movement speed: ${speed}`);
+
+// 3. Реалізуй конвертор валют. Користувач вводить долари, програма переводить їх в євро. Курс валют зберігається в константі.
+// const amountOfDollars = +prompt("Enter the amount of dollars");
+// const euro = 0.8844;
+// const currencyСonversion = amountOfDollars * euro;
+// console.log(`The amount of dollars: ${amountOfDollars}, Currency conversion in euros: ${currencyСonversion}`);
